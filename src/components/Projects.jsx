615752b20009c1fbe392004528fa17c4481c56d4
Ajay <ajayaj2002nov@gmail.com>
2025-02-11 11:08:45 +0530

    last

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ function Projects() {
 
 
           <Col lg={4} md={6} sm={12} style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"40px" }}>
-            <Card className='shadow pro' style={{ width: '18rem',padding:"0px" }}>
+            <Card className='shadow border-0 pro' style={{ width: '18rem',padding:"0px" }}>
               <Card.Img className='' style={{height:"200px"}} variant="top" src="https://img.freepik.com/free-vector/gradient-multigenerational-home-illustration_52683-134517.jpg?t=st=1733717495~exp=1733721095~hmac=242f169f6eca5a5155f54d1e42b0beb6c0f2cd82782c0a70f04818e1a4365093&w=826" />
               <Card.Body>
                 <Card.Title className='text-center my-3'>RealSpace</Card.Title>
@@ -29,7 +29,7 @@ function Projects() {
 
 
           <Col lg={4} md={6} sm={12} style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"40px" }}>
-            <Card className='shadow pro' style={{ width: '18rem',padding:"0px" }}>
+            <Card className='shadow pro border-0' style={{ width: '18rem',padding:"0px" }}>
               <Card.Img style={{height:"200px"}} variant="top" src="https://img.freepik.com/free-vector/customers-with-phones-shopping-online_74855-4781.jpg?t=st=1734414165~exp=1734417765~hmac=552e89203b87126cc2706357601f9cdf988713658f056d3732dc9b1afe6d7275&w=1060" />
               <Card.Body>
                 <Card.Title className='text-center my-3'>E-Kart</Card.Title>
@@ -44,7 +44,7 @@ function Projects() {
           </Col>
 
           <Col lg={4} md={6} sm={12} style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"40px" }} >
-            <Card className='shadow pro' style={{ width: '18rem',padding:"0px" }}>
+            <Card className='shadow border-0 pro' style={{ width: '18rem',padding:"0px" }}>
               <Card.Img style={{height:"200px"}} variant="top" src="https://img.freepik.com/free-vector/schoolboy-standing-books-raising-hand-speaking-pupil-reading-home-task-report-flat-vector-illustration-school-education-knowledge_74855-8576.jpg?t=st=1728199925~exp=1728203525~hmac=42a3f13837531e518f3c20a3fb8b189f0c0298a409060062324f2522a66423d3&w=740" />
               <Card.Body>
                 <Card.Title className='text-center'>Edu Connect</Card.Title>
@@ -60,7 +60,7 @@ function Projects() {
           </Col>
 
           <Col lg={4} md={6} sm={12} style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"40px" }}>
-            <Card className='shadow pro' style={{ width: '18rem',padding:"0px" }}>
+            <Card className='shadow border-0 pro' style={{ width: '18rem',padding:"0px" }}>
               <Card.Img style={{height:"200px"}} variant="top" src="https://img.freepik.com/free-vector/recipe-book-concept-illustration_114360-7856.jpg?t=st=1728200570~exp=1728204170~hmac=62f3b5b853f28fd0137b502a869d96e57a70c99587d68ae6da6322be763bf40a&w=740" />
               <Card.Body>
                 <Card.Title className='text-center my-3'>Recipe Finder</Card.Title>
@@ -76,7 +76,7 @@ function Projects() {
           </Col>
 
           <Col lg={4} md={6} sm={12} style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"40px" }}>
-            <Card className='shadow pro' style={{ width: '18rem',padding:"0px" }}>
+            <Card className='shadow border-0 pro' style={{ width: '18rem',padding:"0px" }}>
               <Card.Img style={{height:"200px"}} variant="top" src="https://img.freepik.com/free-vector/happy-female-farmer-working-farm-feed-population-flat-vector-illustration-cartoon-farm-with-automation-technology_74855-8400.jpg?t=st=1728209215~exp=1728212815~hmac=3603fa1f9870024296da185161130b38b27def2b487ca59df3b3f04a1ac8df96&w=900" />
               <Card.Body>
                 <Card.Title className='text-center'>Fresh Organic</Card.Title>
@@ -92,7 +92,7 @@ function Projects() {
           </Col>
 
           <Col lg={4} md={6} sm={12} style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"40px" }}>
-            <Card className='shadow pro' style={{ width: '18rem',padding:"0px" }}>
+            <Card className='shadow border-0 pro' style={{ width: '18rem',padding:"0px" }}>
               <Card.Img className='mb-5' style={{height:"150px"}} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RACuKQ8sJxNXI17b6tLyd4vmhraPT5OYmw&s" />
               <Card.Body>
                 <Card.Title className='text-center'>Air-Pods Clone</Card.Title>
--- src/components/Skillls.jsx
+++ src/components/Skillls.jsx
@@ -25,7 +25,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
               {/* HTML */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
               <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIxeNFO8PSlvOvudmrtLIOYdTpN6o77VKolQ&s" className=''style={{width:"45%",marginTop:"35px",marginBottom:"20px"}}/>
               <Card.Body>
                 <Card.Title className='text-center'>HTML-5</Card.Title>
@@ -35,7 +35,7 @@ function Skillls() {
 
            <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
               {/* css */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
               <Card.Img variant="top" src={css} className='' style={{width:"60%",marginTop:"20px"}}/>
               <Card.Body>
                 <Card.Title className='text-center'>Css-3</Card.Title>
@@ -45,7 +45,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
             {/* bootstrap */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
               <Card.Img variant="top" src={bootstrap} className='' style={{width:"50%",marginTop:"40px"}}/>
               <Card.Body>
                 <Card.Title className='text-center'>Bootstrap</Card.Title>
@@ -55,7 +55,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
             {/* tailwind */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
               <Card.Img variant="top" src={tailwind} className='' style={{width:"50%",marginTop:"40px"}}/>
               <Card.Body>
                 <Card.Title className='text-center'>Tailwind Css</Card.Title>
@@ -65,7 +65,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
             {/* javascript */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center" }}>
               <Card.Img variant="top" src={js} className='' style={{width:"50%",marginTop:"40px"}}/>
               <Card.Body>
                 <Card.Title className='text-center'>Javascript (ES6+)</Card.Title>
@@ -75,7 +75,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
             {/* Recat */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
               <Card.Img variant="top" src={react} className='' style={{width:"50%",marginTop:"40px"}} />
               <Card.Body>
                 <Card.Title className='text-center'>React-JS</Card.Title>
@@ -85,7 +85,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
             {/* Redux */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
               <Card.Img variant="top" src={redux} className='' style={{width:"50%",marginTop:"40px"}} />
               <Card.Body>
                 <Card.Title className='text-center'>Redux</Card.Title>
@@ -95,7 +95,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
             {/* angular */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
               <Card.Img variant="top" src={angular} className='' style={{width:"50%",marginTop:"40px"}} />
               <Card.Body>
                 <Card.Title className='text-center'>Angular</Card.Title>
@@ -105,7 +105,7 @@ function Skillls() {
 
            <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
              {/* node-js */}
-             <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
+             <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
               <Card.Img variant="top" src={node} className='' style={{width:"50%",marginTop:"40px"}} />
               <Card.Body>
                 <Card.Title className='text-center'>Node-JS</Card.Title>
@@ -115,7 +115,7 @@ function Skillls() {
 
            <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
              {/* express-js */}
-             <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
+             <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
               <Card.Img variant="top" src={express} className='' style={{width:"50%",marginTop:"40px"}} />
               <Card.Body>
                 <Card.Title className='text-center'>Express-JS</Card.Title>
@@ -125,7 +125,7 @@ function Skillls() {
 
           <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
             {/* mongo-db */}
-            <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
+            <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
               <Card.Img variant="top" src="https://img.icons8.com/color/200/mongodb.png" className='' style={{width:"50%",marginTop:"40px"}} />
               <Card.Body>
                 <Card.Title className='text-center'>Mongo-DB</Card.Title>
@@ -135,7 +135,7 @@ function Skillls() {
 
            <Col lg={3} md={6} sm={12} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
              {/* git */}
-             <Card className='shadow mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
+             <Card className='border-0 mt-5 cd1' style={{ width: '15rem', padding: "0px",display:"flex",justifyContent:"center",alignItems:"center"  }}>
               <Card.Img variant="top" src={git} className='' style={{width:"50%",marginTop:"40px"}} />
               <Card.Body>
                 <Card.Title className='text-center'>Git</Card.Title>
